049b5441b3274aa975c3edc1df396942b8dbcac4
Felipe Blanco Muzzolon <felipeblanco114@gmail.com>
2023-09-26 12:06:15 -0300

    first commit

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Benjamin Aguirre',
+  description: 'benjamin aguirre, fotografía',
+}
+
+export default function RootLayout({ children }) {
+ return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
